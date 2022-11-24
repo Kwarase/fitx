c9202c7a29431344680dd20600df93d7082a4a74
Emmanuel Kwarase <112888639+Kwarase@users.noreply.github.com>
2022-11-24 09:09:44 +0000

    Update dbcred.php

--- settings/dbcred.php
+++ settings/dbcred.php
@@ -1,8 +1,8 @@
 <?php
 //Database credentials
-define("DATABASE", "poks");
-define("SERVER", "localhost");
-define("USERNAME", "root");
-define("PASSWD", "");
+define("DATABASE", "heroku_3913570f2761d87");
+define("SERVER", "eu-cdbr-west-03.cleardb.net");
+define("USERNAME", "b26bb63832708b");
+define("PASSWD", "1a6fad73");
 
 ?>
